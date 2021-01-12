e78ac27ce8d18121744431e3aabdba64ac9d6d5b
Aweba13 <fiorella.effio@unmsm.edu.pe>
2021-01-12 12:04:47 +0000

    [ADD] test directory mocha chai

---
+++ test/data.spec.js
@@ -0,0 +1,13 @@
+describe('data', () => {
+
+  it('deberia ser falso', () => {
+    assert.isFunction(()=> {});
+  });
+
+  describe('processCohortData({ cohortData, orderBy, orderDirection, filterBy })', () => {
+
+    it('debería retornar arreglo de usuarios');
+
+  });
+
+});
